61436c96afb82de59e419066142a923ffe0ffb64
Alex2013GitHub <96023225+Alex2013GitHub@users.noreply.github.com>
2023-08-16 17:54:42 +0000

    Deploying to gh-pages from @ Alex2013GitHub/goit-js-hw-09@41de6aba75e0cd93603a99f80bcea331a397da3a 🚀

---
+++ 01-color-switcher.4555081e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){n=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3),o()})),d.addEventListener("click",(function(){clearInterval(n),o()}));let n=null;function o(){e.disabled?(e.disabled=!1,d.disabled=!0):(e.disabled=!0,d.disabled=!1)}
+//# sourceMappingURL=01-color-switcher.4555081e.js.map
